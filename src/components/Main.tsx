1a7434ccd7ce54a526c7894e2ad554265747cb64
Gentil Alyson Jacome Fernandes <fernandes.gentilalysonjac@gmail.com>
2022-08-07 21:22:59 -0300

    fix: left-side article proper styling

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,23 +1,16 @@
 const Main = () => {
   return (
     <main className="h-4/5 flex flex-row justify-between">
-      <article className="basis-2/5 bg-background">
-        <section className="border-solid border-2 border-black h-full flex flex-col justify-center items-center gap-5">
-          <h1 className="text-5xl font-bold max-w-min">
-            Tênis Esportivo Para Corrida Top
-          </h1>
-          <p className="w-auto pl-40 text-text-description font-bold">
+      <article className="basis-2/5 bg-background flex flex-col justify-center items-start">
+        <span className="pl-32 pr-32 font-bold">
+          <h1 className="text-5xl">Tênis Esportivo Para Corrida Top</h1>
+          <p className="text-text-description">
             Esse tênis tem o intuito de te deixar ainda mais fitness, focado e
             musculoso
           </p>
-          <p>
-            R$ 1.000,00
-          </p>
-
-          <button>
-            COMPRAR
-          </button>
-        </section>
+          <p className="text-3xl">R$ 1.000,00</p>
+        </span>
+        <button className="bg-button ml-32 w-48 h-12 text-white font-bold">COMPRAR</button>
       </article>
       <article className="basis-3/5 bg-main-background"></article>
     </main>
